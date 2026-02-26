a08e94344f9fd91d2e48ad235c7f96eface830c8
Yodi <yodi.vandenhende@gmail.com>
2026-02-26 22:57:15 +0100

    abstracting playtest form

---
+++ site/src/lib/components/playtest-form.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	let isFormVisable = $state(false);
+
+	function showForm() {
+		isFormVisable = true;
+	}
+
+</script>
+
+<main>
+	{#if isFormVisable === false}
+		<button onclick={showForm}>Join de Federatie</button>
+	{/if}
+	{#if isFormVisable}
+        <div class="form">
+			<div class="input">
+				<label for="playtest-name">Naam:</label>
+				<input type="text" id="playtest-name" name="playtest-name" value="Yodi" />
+			</div>
+			<div class="input">
+				<label for="playtest-email">Email:</label>
+				<input type="email" id="playtest-email" name="playtest-email" value="test@test.com" />
+			</div>
+			<button>Verstuur aplicatie</button>
+        </div>
+	{/if}
+</main>
+
+<style>
+
+	.input {
+		display: flex;
+		align-items: center;
+		width: 100%;
+	}
+
+	.input input {
+		width: 100%;
+		margin-left: 0.5em;
+		border: 0;
+		border-bottom: 2px solid var(--custom-green);
+		outline: none;
+		font-size: 1.2em;
+		color: var(--font-green);
+		background-color: black;
+	}
+
+	button {
+		margin-top: 1em;
+		padding: 0.5em 1em;
+		border: 2px solid var(--custom-green);
+		border-radius: 5px;
+		font-size: 1em;
+		color: var(--font-green);
+		background-color: black;
+		cursor: pointer;
+	}
+
+	button:active {
+		background-color: var(--custom-green);
+		color: black;
+	}
+</style>
--- site/src/routes/info/+page.svelte
+++ site/src/routes/info/+page.svelte
@@ -2,65 +2,58 @@
 	import { enhance } from '$app/forms';
 	import logo from '$lib/assets/images/Logo.png';
 	import CodeScroller from '$lib/components/code-scroller.svelte';
+	import PlaytestForm from '$lib/components/playtest-form.svelte';
 
-	const logoAnimationDuration = 3;
+	const logoAnimationDuration = 1;
 	const contentAnimationDuration = 2;
 	let startCode = $state(false);
 
-	setTimeout(() => startCode = true , (logoAnimationDuration + contentAnimationDuration) * 1000);
+	setTimeout(() => (startCode = true), (logoAnimationDuration + contentAnimationDuration) * 1000);
 
 	function postSubmit() {
 		return () => {};
 	}
 </script>
 
-<main style="--logo-animation-duration: {logoAnimationDuration}s; --content-animation-duration: {contentAnimationDuration}s">
-<div class="code">
-	<CodeScroller start={startCode} speed={5} />
-</div>
-<img src={logo} alt="TerraPrime Logo" class="logo" />
-<div class="grid">
-	<section class="glow-border grow-animation" style="">
-		<div class="content">
-			<p>Terra Prime is een gloednieuwe volwassenen-LARP onder DHvT.</p>
-			<p>
-				Stap in een futuristische wereld waar actie, samenwerking en verbeelding centraal staan.
-				Samen met andere spelers en crew creëer je een meeslepend verhaal vol avontuur, spanning en
-				humor !
-			</p>
-			<p>
-				We spelen om elkaar te versterken, niet om te winnen. Daarom houden we de regels eenvoudig
-				en ligt de focus op sfeer, plezier en sterk rollenspel. Nieuwe deelnemers kunnen vlot
-				instappen, terwijl ervaren spelers alle ruimte krijgen om te schitteren in hun personage.
-			</p>
-			<p>
-				Verwacht sciencefictionvibes, Nerf, close combat en een beleving die je even helemaal
-				losmaakt van de echte wereld.
-			</p>
-			<p>Jouw plaats wacht in de Federatie!</p>
+<main
+	style="--logo-animation-duration: {logoAnimationDuration}s; --content-animation-duration: {contentAnimationDuration}s"
+>
+	<div class="code">
+		<CodeScroller start={startCode} speed={5} />
+	</div>
+	<img src={logo} alt="TerraPrime Logo" class="logo" />
+	<div class="grid">
+		<section class="glow-border grow-animation" style="">
+			<div class="content">
+				<p>Terra Prime is een gloednieuwe volwassenen-LARP onder DHvT.</p>
+				<p>
+					Stap in een futuristische wereld waar actie, samenwerking en verbeelding centraal staan.
+					Samen met andere spelers en crew creëer je een meeslepend verhaal vol avontuur, spanning
+					en humor !
+				</p>
+				<p>
+					We spelen om elkaar te versterken, niet om te winnen. Daarom houden we de regels eenvoudig
+					en ligt de focus op sfeer, plezier en sterk rollenspel. Nieuwe deelnemers kunnen vlot
+					instappen, terwijl ervaren spelers alle ruimte krijgen om te schitteren in hun personage.
+				</p>
+				<p>
+					Verwacht sciencefictionvibes, Nerf, close combat en een beleving die je even helemaal
+					losmaakt van de echte wereld.
+				</p>
+				<p>Jouw plaats wacht in de Federatie!</p>
 
-
-			<form method="post" use:enhance={postSubmit}>
-				<p>Deel namen met de playtest ?</p>
-				<div class="input">
-					<label for="playtest-name">Naam:</label>
-					<input type="text" id="playtest-name" name="playtest-name" value="Yodi" />
-				</div>
-				<div class="input">
-					<label for="playtest-email">Email:</label>
-					<input type="email" id="playtest-email" name="playtest-email" value="test@test.com" />
-				</div>
-				<button>Join de Federatie</button>
-			</form>
-		</div>
-	</section>
+				<form method="post" use:enhance={postSubmit} >
+					<PlaytestForm />
+				</form>
+			</div>
+		</section>
 	</div>
 </main>
 
 <style>
 	:root {
 		--custom-green: #00aa00;
-		--font-green: #008800; 
+		--font-green: #008800;
 	}
 	main {
 		width: 100vw;
@@ -69,7 +62,7 @@
 		position: relative;
 		background-color: black;
 	}
-	
+
 	.code {
 		position: absolute;
 		top: 0;
@@ -84,9 +77,9 @@
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-template-rows: min(310px, 70vw) min-content;
-		grid-template-areas: 
-		"logo"
-		"section";
+		grid-template-areas:
+			'logo'
+			'section';
 		justify-items: center;
 		align-items: start;
 		width: 100vw;
@@ -94,7 +87,6 @@
 		overflow-x: hidden;
 		scrollbar-color: var(--custom-green) black;
 		background-color: black;
-
 	}
 
 	.logo {
@@ -116,14 +108,14 @@
 			opacity: 0;
 			max-width: 80vw;
 			max-height: 80vh;
-			top:50%;
+			top: 50%;
 			transform: translate(-50%, -50%);
 		}
 		50% {
 			opacity: 1;
 			max-width: 80vw;
 			max-height: 80vh;
-			top:50%;
+			top: 50%;
 			transform: translate(-50%, -50%);
 		}
 		100% {
@@ -134,7 +126,7 @@
 			transform: translate(-50%, 0);
 		}
 	}
-	
+
 	section {
 		grid-area: section;
 		z-index: 2;
@@ -167,7 +159,7 @@
 			opacity: 0;
 			padding: 0;
 			width: 0;
-			height: 0; 
+			height: 0;
 		}
 		5% {
 			opacity: 1;
@@ -193,38 +185,4 @@
 		font-family: 'Science Gothic', 'Courier New', Courier, monospace;
 		margin-bottom: 1rem;
 	}
-
-	.input {
-		display: flex;
-		align-items: center;
-		width: 100%;
-	}
-
-	.input input {
-		width: 100%;
-		margin-left: 0.5em;
-		border: 0;
-		border-bottom: 2px solid var(--custom-green);
-		outline: none;
-		font-size: 1.2em;
-		color: var(--font-green);
-		background-color: black;
-	}
-
-	button {
-		margin-top: 1em;
-		padding: 0.5em 1em;
-		border: 2px solid var(--custom-green);
-		border-radius: 5px;
-		font-size: 1em;
-		color: var(--font-green);
-		background-color: black;
-		cursor: pointer;
-	}
-
-	button:active {
-		background-color: var(--custom-green);
-		color: black;
-	}
-
 </style>

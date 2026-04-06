08bfb5e43098740845aa75b13cf0ec0e4e66dd4d
yodi vandenhende <yodi.vandenhende@gmail.com>
2026-04-06 15:00:33 +0200

    adding structure for displaying on desktop

---
+++ site/src/lib/codex/components/window.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { type CodexWindow } from '$lib/codex/services/window-service.svelte';
+	let { window }: { window: CodexWindow } = $props();
+</script>
+
+<main></main>
+
+<style>
+</style>
--- site/src/lib/codex/services/window-service.svelte.ts
+++ site/src/lib/codex/services/window-service.svelte.ts
@@ -1,15 +1,24 @@
 function createWindowService() {
-  const windows = $state([]);
+  let windows = $state([] as CodexWindow[]);
   const openWindows = $derived(windows.filter(window => window.state === 'open'));
+
+  function openWindow(id: string) {
+    windows = windows.map(window => {
+      if (window.id === id) window.state = 'open';
+      return window
+    })
+  }
 
   return {
     openWindows,
+    openWindow,
 
   }
 }
 
 export const windowService = createWindowService();
-export type Window = {
+
+export type CodexWindow = {
   id: string;
   state: 'open' | 'hidden' | 'closed';
 }

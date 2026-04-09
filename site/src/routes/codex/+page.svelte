5f9fecee76c894651160a5fa9b9a643c391e8b35
yodivdhende <yodi.vandenhende@gmail.com>
2026-04-09 13:12:27 +0200

    fine tunining crt effect

--- site/src/routes/codex/+page.svelte
+++ site/src/routes/codex/+page.svelte
@@ -3,12 +3,14 @@
 	import Taskbar from '$lib/codex/components/taskbar.svelte';
 </script>
 
-<main class="crt-perspective">
-	<Desktop></Desktop>
-	<Taskbar></Taskbar>
-</main>
-<div class="vignette"></div>
-<div class="scanlines"></div>
+<div class="crt-perspective">
+	<main>
+		<Desktop></Desktop>
+		<Taskbar></Taskbar>
+	</main>
+	<div class="vignette"></div>
+	<div class="scanlines"></div>
+</div>
 
 <style>
 	@import '$lib/styles/stemtest.css';
@@ -16,42 +18,52 @@
 	main {
 		display: flex;
 		flex-direction: column;
-		width: 100vw;
-		height: 100vh;
+		width: 100%;
+		height: 100%;
 		overflow: hidden;
 		background-color: var(--bg);
 		font-family: 'Courier New', Courier, monospace;
-		transform: rotateX(1deg);
 		border-radius: 4px;
+		transform: rotateX(1deg);
 		transform-origin: center center;
 	}
 
-	/* .crt-perspective { */
-	/* 	perspective: 800px; */
+	.crt-perspective {
+		position: relative;
+		width: 100vw;
+		height: 100vh;
+		perspective-origin: center;
+		perspective: 500px;
+		overflow: hidden;
+		transform-style: preserve-3d;
+		background-color: black;
+	}
 
-	/* } */
+	.vignette {
+		position: absolute;
+		inset: 0;
+		pointer-events: none;
+		background: radial-gradient(ellipse at center, transparent 60%, rgba(0, 0, 0, 0.6) 100%);
+		z-index: 9999;
+		transform: rotateX(1deg);
+		transform-origin: center center;
+	}
 
-	/* .vignette { */
-	/* 	position: absolute; */
-	/* 	inset: 0; */
-	/* 	pointer-events: none; */
-	/* 	background: radial-gradient(ellipse at center, transparent 60%, rgba(0, 0, 0, 0.6) 100%); */
-	/* 	z-index: 9999; */
-	/* } */
-
-	/* .scanlines { */
-	/* 	pointer-events: none; */
-	/* 	position: fixed; */
-	/* 	inset: 0; */
-	/* 	z-index: 10; */
-	/* 	opacity: 1; */
-	/* 	transition: opacity 1s ease; */
-	/* 	background: repeating-linear-gradient( */
-	/* 		to bottom, */
-	/* 		transparent 0px, */
-	/* 		transparent 3px, */
-	/* 		rgba(0, 0, 0, 0.15) 3px, */
-	/* 		rgba(0, 0, 0, 0.15) 4px */
-	/* 	); */
-	/* } */
+	.scanlines {
+		pointer-events: none;
+		position: fixed;
+		inset: 0;
+		z-index: 10;
+		opacity: 1;
+		transition: opacity 1s ease;
+		background: repeating-linear-gradient(
+			to bottom,
+			transparent 0px,
+			transparent 3px,
+			rgba(0, 0, 0, 0.15) 3px,
+			rgba(0, 0, 0, 0.15) 4px
+		);
+		transform: rotateX(1deg);
+		transform-origin: center center;
+	}
 </style>

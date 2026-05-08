3ff6ea91a16b607dfef0383816e2db17030218bf
yodivdhende <yodi.vandenhende@gmail.com>
2026-05-08 12:04:46 +0200

    resetting promopage

--- site/src/routes/promo/+page.svelte
+++ site/src/routes/promo/+page.svelte
@@ -11,15 +11,26 @@
 		timeLeft = getTimeLeft();
 	}, 1000);
 
+	$effect(() => {
+		if (timeLeft !== null && timeLeft <= 0) {
+			fadeOut.set(2, { duration: 2000});
+		}
+	});
+
+	$effect(() => {
+		if (fadeOut.current === 2) goto('/info');
+	})
+
 	function getTimeLeft(): number {
 		const now = new Date();
 		const difference = Math.floor((targetDate.getTime() - now.getTime()) / 1000);
 		if (difference < 0) {
 			targetDate = new Date(new Date().getTime() + 6000); // Reset for testingjk
-			return getTimeLeft() - 1;
-		}
+			return getTimeLeft()-1;
+		};
 		return difference;
 	}
+
 </script>
 
 <main>
@@ -47,5 +58,6 @@
 		height: 100%;
 		background-color: black;
 		pointer-events: none;
+
 	}
 </style>

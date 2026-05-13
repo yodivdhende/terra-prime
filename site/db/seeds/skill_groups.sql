8264eecab3745b22adac22c6c47c54fb41c5b1ff
Yodi <yodi.vandenhende@gmail.com>
2026-05-13 15:05:38 +0200

    filling in skills

--- site/db/seeds/skill_groups.sql
+++ site/db/seeds/skill_groups.sql
@@ -1,6 +1,5 @@
 INSERT INTO `Skill_Groups` (`Id`, `Name`, `Description`) VALUES
-(1, 'Enginerring', 'Creating and repairing stuf'),
-(2, 'Medicen', 'Healing stuf'),
-(3, 'Infromatics', 'Computer stuf'),
-(4, 'Research', 'looking into stuf'),
-(5, 'Servival', 'Keep on living');
+(1, 'Engineering', 'Technische kennis voor het bouwen, repareren en onderhouden van mechanische en elektrische systemen.'),
+(2, 'Life Sciences', 'Wetenschappelijke kennis van het menselijk lichaam, chemische stoffen en natuurlijke omgevingen.'),
+(3, 'Information Technology', 'Digitale expertise in het analyseren, programmeren en beheren van computersystemen en communicatienetwerken.'),
+(4, 'Sociale wetenschap', 'Kennis van geschiedenis, samenlevingen en politieke structuren om de wereld en haar facties te begrijpen.');

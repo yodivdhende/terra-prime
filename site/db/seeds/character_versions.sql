5e0aa9d68cfebbdaa362b00899d8761825bc191a
yodi vandenhende <yodi.vandenhende@gmail.com>
2026-05-23 11:47:27 +0200

    update seed to add companies to character versions

--- site/db/seeds/character_versions.sql
+++ site/db/seeds/character_versions.sql
@@ -1,6 +1,6 @@
-INSERT INTO `Character_Versions` (`Id`, `Character`, `Name`) VALUES
-(1, 1, 'Version 1'),
-(2, 1, 'Version 2'),
-(3, 2, 'Version 1'),
-(4, 3, 'Version 1'),
-(5, 4, 'Version 1');
+INSERT INTO `Character_Versions` (`Id`, `Character`, `Name`, `Company`) VALUES
+(1, 1, 'Version 1', 1),
+(2, 1, 'Version 2', 2),
+(3, 2, 'Version 1', 3),
+(4, 3, 'Version 1', 4),
+(5, 4, 'Version 1', 5);

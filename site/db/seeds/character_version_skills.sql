a805c2e124875b0676ca13b3fe87cdae13359ee1
Yodi <yodi.vandenhende@gmail.com>
2026-05-31 00:10:14 +0200

    fixing seeding and migrations

--- site/db/seeds/character_version_skills.sql
+++ site/db/seeds/character_version_skills.sql
@@ -1,7 +1,7 @@
 INSERT INTO `Character_Version_Skills` (`Id`, `CharacterVersion`, `Skill`, `Value`) VALUES
-(1, 1, 1, 10),
-(2, 1, 2, 40),
-(3, 1, 3, 50),
+(1, 1, 1, 1),
+(2, 1, 2, 4),
+(3, 1, 3, 5),
 (4, 2, 1, 1),
 (5, 2, 2, 1),
 (6, 2, 4, 1),
--- site/db/seeds/event_character_discounts_skills.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `Event_Character_Discounts_Skills` (`Event`, `Character`, `Skill`, `Discount`) VALUES
-(1, 1, 2, 5),
-(1, 2, 3, 5);

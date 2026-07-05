02fb897233aa3f98f054092266821355e077f0c5
Yodi <yodi.vandenhende@gmail.com>
2026-07-05 16:20:52 +0200

    adjusting costing

--- site/db/seeds/implants.sql
+++ site/db/seeds/implants.sql
@@ -1,10 +1,12 @@
 INSERT INTO `Implants` (`Id`, `Name`, `Description`, `Cost`) VALUES
-(1, 'Combat Medic', '1 charge = 1 stervende persoon terug tot levend wekken. (3 charges)', 20),
-(2, 'Extra Prints brons', 'je krijgt 1 extra print', 10),
-(3, 'Extra Prints zilver', 'je krijgt 2 extra print', 10),
-(4, 'Extra Prints goud', 'je krijgt 3 extra print', 10),
-(5, 'Internal carapace brons', 'je mag 1 hit negeren', 20),
-(6, 'Internal carapace zilver', 'je mag 1 extra hit negeren', 10),
-(7, 'Internal carapace gold', 'je mag 1 extra hit negeren', 10),
-(8, 'Local Reconstruction Protocal', 'Je mag 1 print gebruiken om terplaatse te printen', 30),
-(9, 'Virtuele recon', 'Je kan van afde printer 20 secconden rond lopen zonder geraakt te worden. tijdens deze 20 seccond kan je zelf geen schade aan richten. Na de 20 secconden moet je rechtstreeks terug naar de printer gaan.', 50);
+(1, 'Combat Medic brons', '1 charge = 1 stervende persoon terug tot levend wekken. (1 charges)', 1100),
+(2, 'Combat Medic zilver', '1 charge = 1 stervende persoon terug tot levend wekken. (3 charges)', 1300),
+(3, 'Combat Medic gold', '1 charge = 1 stervende persoon terug tot levend wekken. (5 charges)', 1500),
+(4, 'Extra Prints brons', 'je krijgt 1 extra print', 1000),
+(5, 'Extra Prints zilver', 'je krijgt 2 extra print', 1200),
+(6, 'Extra Prints goud', 'je krijgt 3 extra print', 1400),
+(7, 'Internal carapace brons', 'je mag 1 hit negeren', 1000),
+(8, 'Internal carapace zilver', 'je mag 1 extra hit negeren', 1100),
+(9, 'Internal carapace gold', 'je mag 1 extra hit negeren', 1200),
+(10, 'Local Reconstruction Protocal', 'Je mag 1 print gebruiken om terplaatse te printen', 1300),
+(11, 'Virtuele recon', 'Je kan van afde printer 20 secconden rond lopen zonder geraakt te worden. tijdens deze 20 seccond kan je zelf geen schade aan richten. Na de 20 secconden moet je rechtstreeks terug naar de printer gaan.', 1500);
